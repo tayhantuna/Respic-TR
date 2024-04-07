8522529b33138f0ed53b9b86022b95a912652e1e
tayhantuna <158285886+tayhantuna@users.noreply.github.com>
2024-04-07 14:23:20 +0000

    Op items

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 var videoPlayer = document.getElementById("videoPlayer");
+var all = document.getElementById('podcasts');
+
 function showSection(sectionId) {
     var sections = document.getElementsByTagName("section");
     for (var i = 0; i < sections.length; i++) {
         sections[i].style.display = "none";
     }
     document.getElementById(sectionId).style.display = "block";
-videoPlayer.pause();
+    videoPlayer.pause();
 }
 
-function changeVideo(videoSrc,tn) {
+function changeVideo(videoSrc, tn) {
     videoPlayer.src = videoSrc;
-  videoPlayer.poster = tn;
+    videoPlayer.poster = tn;
     videoPlayer.load();
-  videoPlayer.play();
+    videoPlayer.play();
+  var sectio = document.getElementsByTagName("section");
+    for (var i = 0; i < sectio.length; i++) {
+        sectio[i].style.display = "none";
+    }
+    all.style.display = "block";
 }
